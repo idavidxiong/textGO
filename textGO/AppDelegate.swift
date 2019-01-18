9056ac788faa9cfb22774e403462f3ae27b98246
5km <5km@smslit.cn>
2019-01-18 23:49:09 +0800

    Initial Commit

---
+++ textGO/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  textGO
+//
+//  Created by 5km on 2019/1/18.
+//  Copyright © 2019 5km. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
