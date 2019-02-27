36c5c46d6e6e54bdfe42d39f69cf4959ccfd8551
5km <5km@smslit.cn>
2019-02-27 15:47:56 +0800

    NSTextField支持撤销和重做快捷键

--- textGO/textGoPublic.swift
+++ textGO/textGoPublic.swift
@@ -26,9 +26,10 @@ func tipInfo(withTitle: String, withMessage: String) {
 // NSTextField 支持快捷键
 extension NSTextField {
     open override func performKeyEquivalent(with event: NSEvent) -> Bool {
-        if !event.isCommandDown {
+        if event.modifierFlags.isDisjoint(with: .command) {
             return super.performKeyEquivalent(with: event)
         }
+       
         switch event.charactersIgnoringModifiers {
         case "a":
             return NSApp.sendAction(#selector(NSText.selectAll(_:)), to: self.window?.firstResponder, from: self)
@@ -38,14 +39,14 @@ extension NSTextField {
             return NSApp.sendAction(#selector(NSText.paste(_:)), to: self.window?.firstResponder, from: self)
         case "x":
             return NSApp.sendAction(#selector(NSText.cut(_:)), to: self.window?.firstResponder, from: self)
+        case "z":
+            self.window?.firstResponder?.undoManager?.undo()
+            return true
+        case "Z":
+            self.window?.firstResponder?.undoManager?.redo()
+            return true
         default:
             return super.performKeyEquivalent(with: event)
         }
-    }
-}
-
-extension NSEvent {
-    var isCommandDown: Bool {
-        return (self.modifierFlags.rawValue & NSEvent.ModifierFlags.command.rawValue) != 0
     }
 }
